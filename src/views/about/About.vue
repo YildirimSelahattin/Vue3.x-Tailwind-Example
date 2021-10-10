2a4c8a7b53f4fcfb9278a799b7e7d25d1e376d1c
YildirimSelahattin <selahattin_yildirim@outlook.com>
2021-10-10 03:52:23 +0300

    HomePageRevised

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -1,36 +1,49 @@
 <template>
   <div class="about">
-    <TopSection />
-        <section class="bg-gray-900 py-8">
-
-        <div class="container py-8 px-6 mx-auto">
-
-            <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-100 text-xl mb-8"
-                href="#">
-                Hakkımızda
-            </a>
-            <p class="my-8 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan lacus vel
-                facilisis volutpat. Vitae aliquet nec ullamcorper sit. Nullam eget felis eget nunc lobortis mattis
-                aliquam. In est ante in nibh mauris. Egestas congue quisque egestas diam in. Facilisi nullam vehicula
-                ipsum a arcu. Nec nam aliquam sem et tortor consequat. Eget mi proin sed libero enim sed faucibus turpis
-                in. Hac habitasse platea dictumst quisque. In aliquam sem fringilla ut. Gravida rutrum quisque non
-                tellus orci ac auctor augue mauris. Accumsan lacus vel facilisis volutpat est velit egestas dui id. At
-                tempor commodo ullamcorper a. Volutpat commodo sed egestas egestas fringilla. Vitae congue eu consequat
-                ac.</p>
-
-        </div>
-
+    <div :class="[$route.name == 'Home' ? 'hidden' : '']">
+      <TopSection />
+    </div>
+    <section class="bg-gray-900 py-8 " :class="[$route.name == 'Home' ? 'bg-gray-900 py-8 border-t-2 border-yellow-700' : '']">
+      <div class="container py-8 px-6 mx-auto">
+        <a
+          class="
+            uppercase
+            tracking-wide
+            no-underline
+            hover:no-underline
+            font-bold
+            text-gray-100 text-xl
+            mb-8
+          "
+          href="#"
+        >
+          Hakkımızda
+        </a>
+        <p class="my-8 text-gray-400">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus
+          commodo viverra maecenas accumsan lacus vel facilisis volutpat. Vitae
+          aliquet nec ullamcorper sit. Nullam eget felis eget nunc lobortis
+          mattis aliquam. In est ante in nibh mauris. Egestas congue quisque
+          egestas diam in. Facilisi nullam vehicula ipsum a arcu. Nec nam
+          aliquam sem et tortor consequat. Eget mi proin sed libero enim sed
+          faucibus turpis in. Hac habitasse platea dictumst quisque. In aliquam
+          sem fringilla ut. Gravida rutrum quisque non tellus orci ac auctor
+          augue mauris. Accumsan lacus vel facilisis volutpat est velit egestas
+          dui id. At tempor commodo ullamcorper a. Volutpat commodo sed egestas
+          egestas fringilla. Vitae congue eu consequat ac.
+        </p>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
-import TopSection from '@/components/TopSection'
+import TopSection from "@/components/TopSection";
 export default {
-  name : "About",
+  name: "About",
   components: {
-    TopSection
-  }
-}
+    TopSection,
+  },
+};
 </script>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,294 @@
 <template>
   <div class="home bg-gray-800">
     <Carousel />
+
+    <!-- Componentlere ayır -->
+    <div class="border-t-2 w-full border-yellow-700"></div>
+    <div
+      class="
+        max-w-screen-xl
+        p-4
+        pb-14
+        bg-gray-800
+        mx-auto
+        px-4
+        sm:px-6
+        lg:px-8
+        relative
+        py-26
+        lg:mt-10
+      "
+    >
+      <div class="relative">
+        <div
+          class="
+            lg:grid
+            lg:grid-flow-row-dense
+            lg:grid-cols-2
+            lg:gap-8
+            lg:items-center
+          "
+        >
+          <div class="lg:col-start-2 lg:max-w-2xl ml-auto">
+            <p class="text-2xl leading-6 font-bold text-yellow-700">
+              Ürünlerimiz
+            </p>
+            <h4
+              class="
+                mt-2
+                text-xl
+                leading-8
+                font-extrabold
+                text-gray-400
+                dark:text-white
+                sm:text-xl sm:leading-9
+              "
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h4>
+            <p class="mt-4 text-lg leading-6 text-gray-500 dark:text-gray-300">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem officiis optio, voluptatem dolore reiciendis
+              consequuntur laudantium aperiam autem sunt libero aliquam sapiente
+              voluptas dolorem minus.
+            </p>
+            <ul class="mt-8 md:grid md:grid-cols-2 gap-6">
+              <li class="mt-6 lg:mt-0">
+                <div class="flex">
+                  <span
+                    class="
+                      flex-shrink-0 flex
+                      items-center
+                      justify-center
+                      h-6
+                      w-6
+                      rounded-full
+                      bg-green-100
+                      text-green-800
+                      dark:text-green-500
+                      drark:bg-transparent
+                    "
+                  >
+                    <svg
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span
+                    class="
+                      ml-4
+                      text-base
+                      leading-6
+                      font-medium
+                      text-gray-500
+                      dark:text-gray-200
+                    "
+                  >
+                    Ücretsiz kurulum
+                  </span>
+                </div>
+              </li>
+              <li class="mt-6 lg:mt-0">
+                <div class="flex">
+                  <span
+                    class="
+                      flex-shrink-0 flex
+                      items-center
+                      justify-center
+                      h-6
+                      w-6
+                      rounded-full
+                      bg-green-100
+                      text-green-800
+                      dark:text-green-500
+                      drark:bg-transparent
+                    "
+                  >
+                    <svg
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span
+                    class="
+                      ml-4
+                      text-base
+                      leading-6
+                      font-medium
+                      text-gray-500
+                      dark:text-gray-200
+                    "
+                  >
+                    Mimari Danışmanlık
+                  </span>
+                </div>
+              </li>
+              <li class="mt-6 lg:mt-0">
+                <div class="flex">
+                  <span
+                    class="
+                      flex-shrink-0 flex
+                      items-center
+                      justify-center
+                      h-6
+                      w-6
+                      rounded-full
+                      bg-green-100
+                      text-green-800
+                      dark:text-green-500
+                      drark:bg-transparent
+                    "
+                  >
+                    <svg
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span
+                    class="
+                      ml-4
+                      text-base
+                      leading-6
+                      font-medium
+                      text-gray-500
+                      dark:text-gray-200
+                    "
+                  >
+                    7/24 destek
+                  </span>
+                </div>
+              </li>
+              <li class="mt-6 lg:mt-0">
+                <router-link to="/product" class="flex">
+                  <span
+                    class="
+                      hidden
+                      flex-shrink-0 flex
+                      items-center
+                      justify-center
+                      h-6
+                      w-6
+                      rounded-full
+                      bg-green-100
+                      text-green-800
+                      dark:text-green-500
+                      drark:bg-transparent
+                    "
+                  >
+                    <svg
+                      class="h-4 w-4"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span
+                    class="
+                      text-lg
+                      hover:underline hover:text-yellow-700
+                      leading-6
+                      font-medium
+                      text-yellow-500
+                      dark:text-gray-200
+                    "
+                  >
+                    Tüm ürünleri gör...
+                  </span>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+          <div
+            class="mt-10 lg:-mx-4 relative relative-20 lg:mt-0 lg:col-start-1"
+          >
+            <div class="relative space-y-4">
+              <div
+                class="flex items-end justify-center lg:justify-start space-x-4"
+              >
+                <img
+                  class="rounded-lg shadow-lg w-32 md:w-56"
+                  width="200"
+                  src="/img/mutfak.jpg"
+                  alt="1"
+                />
+                <img
+                  class="rounded-lg shadow-lg w-40 md:w-64"
+                  width="260"
+                  src="/img/ray.jpg"
+                  alt="2"
+                />
+              </div>
+              <div
+                class="
+                  flex
+                  items-start
+                  justify-center
+                  lg:justify-start
+                  space-x-4
+                  ml-12
+                "
+              >
+                <img
+                  class="rounded-lg shadow-lg w-24 md:w-40"
+                  width="170"
+                  src="/img/koltuk.jpg"
+                  alt="3"
+                />
+                <img
+                  class="rounded-lg shadow-lg w-32 md:w-56"
+                  width="200"
+                  src="/img/oda.jpg"
+                  alt="4"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- About -->
+    <About />
+
   </div>
 </template>
 
 <script>
 import Carousel from "@/components/share/Carousel";
+import About from "@/views/about/About";
 
 export default {
   name: "Home",
   components: {
-    Carousel
+    Carousel,
+    About,
+
   },
 };
 </script>
