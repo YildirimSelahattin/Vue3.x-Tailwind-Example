096e2e79d4b6abbd073d68afa9cb1a6e0965c28e
YildirimSelahattin <selahattin_yildirim@outlook.com>
2021-10-11 00:51:44 +0300

    OurProductsSectionComponents

---
+++ src/components/share/HomeLeftSection.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="
+      mt-10
+      mb-12
+      lg:mb-0 lg:-mx-4
+      relative relative-20
+      lg:mt-0 lg:col-start-1
+    "
+  >
+    <div class="relative space-y-4">
+      <div class="flex items-end justify-center lg:justify-start space-x-4">
+        <img
+          class="rounded-lg shadow-lg w-32 md:w-56"
+          width="200"
+          src="/img/mutfak.jpg"
+          alt="1"
+        />
+        <img
+          class="rounded-lg shadow-lg w-40 md:w-64"
+          width="260"
+          src="/img/ray.jpg"
+          alt="2"
+        />
+      </div>
+      <div
+        class="flex items-start justify-center lg:justify-start space-x-4 ml-12"
+      >
+        <img
+          class="rounded-lg shadow-lg w-24 md:w-40"
+          width="170"
+          src="/img/koltuk.jpg"
+          alt="3"
+        />
+        <img
+          class="rounded-lg shadow-lg w-32 md:w-56"
+          width="200"
+          src="/img/oda.jpg"
+          alt="4"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeLeftSection",
+  components: {
+
+  },
+};
+</script>
+
+<style></style>
